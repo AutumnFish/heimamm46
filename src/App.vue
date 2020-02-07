95dc0687bb1c1f3592c5f8c5256d5fb1c0083bee
autumnFish <517729329@qq.com>
2020-02-07 14:14:43 +0800

    项目起步 - 整合element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link to="info">路由的链接</router-link>
+  <el-button type="success">测试用按钮</el-button>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import App from './App.vue'
 // 导入路由
 import router from './router/index.js'
 
+// 导入 element-ui
+import ElementUI from 'element-ui'
+// 导入 element-ui的样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 注册element-ui
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 
