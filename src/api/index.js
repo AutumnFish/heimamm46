8b41ccf2c8b0e9b5f74933751188ebcb7207a944
autumnFish <517729329@qq.com>
2020-02-11 16:45:57 +0800

    首页模块 - 接口抽取

---
+++ src/api/index.js
@@ -0,0 +1,36 @@
+// 导入 axios
+import axios from 'axios';
+// 导入token工具函数
+import { getToken} from '../utils/token.js'
+
+const indexRequest = axios.create({
+    // 基地址
+    baseURL:process.env.VUE_APP_URL,
+    // 是否跨域
+    withCredentials:true
+})
+
+// 用户信息 接口
+export function info(data) {
+  return indexRequest({
+      url:"/info",
+      method:"get",
+      // 设置请求头
+      // headers 是axios定义的设置方式
+      headers:{
+        token:getToken()
+      }
+  })
+}
+// 用户的退出 接口
+export function logout(data) {
+  return indexRequest({
+      url:"/logout",
+      method:"get",
+      // 设置请求头
+      // headers 是axios定义的设置方式
+      headers:{
+        token:getToken()
+      }
+  })
+}
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,6 +1,17 @@
 <template>
   <el-container class="my-container">
-    <el-header class="my-header">Header</el-header>
+    <el-header class="my-header">
+      <div class="left">
+        <i class="el-icon-s-fold"></i>
+        <img src="../../assets/index_logo.png" alt="">
+        <span>黑马面面</span>
+      </div>
+      <div class="right">
+        <img src="../../assets/logo.png" alt="">
+        <span class='name'>阿达,您好</span>
+        <el-button type="primary">退出</el-button>
+      </div>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="my-aside">Aside</el-aside>
       <el-main class="my-main">Main</el-main>
@@ -16,7 +27,36 @@ export default {};
 .my-container{
     height: 100%;
     .my-header{
-        background: hotpink;
+        // background: hotpink;
+        height: 60px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .left{
+          display: flex;
+          align-items: center;
+          i{
+            font-size:24px;
+            margin-right: 22px;
+          }
+          img{
+            margin-right: 11px;
+          }
+        }
+        .right{
+          display: flex;
+          align-items: center;
+          img{
+            width: 43px;
+            height: 43px;
+            border-radius: 50%;
+            margin-right: 9px;
+          }
+          span.name{
+            margin-right: 38px;
+            font-size:14px;
+          }
+        }
     }
     .my-aside{
         background: yellowgreen;
