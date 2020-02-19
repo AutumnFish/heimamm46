7eacdc15a5d90545ce516dfe166c355c6c3c56c5
autumnFish <517729329@qq.com>
2020-02-19 09:31:34 +0800

    题库模块 - 顶部布局

--- src/views/index/question/question.vue
+++ src/views/index/question/question.vue
@@ -1,13 +1,95 @@
 <template>
-  <div class='question-container'>题库列表</div>
+  <div class="question-container">
+    <el-card class="top-card">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="学科">
+          <el-select v-model="formInline.region" placeholder="请选择学科">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="阶段">
+          <el-select v-model="formInline.region" placeholder="请选择阶段">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="企业">
+          <el-select v-model="formInline.region" placeholder="请选择企业">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="题型">
+          <el-select v-model="formInline.region" placeholder="请选择题型">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <br />
+        <el-form-item label="难度">
+          <el-select v-model="formInline.region" placeholder="请选择难度">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="作者">
+          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-select v-model="formInline.region" placeholder="请选择状态">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期">
+          <el-input v-model="formInline.user" placeholder="选择日期"></el-input>
+        </el-form-item>
+        <br />
+        <el-form-item class="title-item" label="标题">
+          <el-input v-model="formInline.user" placeholder="选择日期"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">搜索</el-button>
+          <el-button>清除</el-button>
+          <el-button type="primary" icon="el-icon-plus">新增试题</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"question"
-}
+  name: 'question',
+  data() {
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      }
+    };
+  }
+};
 </script>
 
-<style>
-
+<style lang="less">
+.question-container {
+  //  所有的盒子 除了最后一个 都设置宽度
+  // .el-form--inline .el-form-item__content {
+  .el-form--inline .el-form-item:not(:last-child) .el-form-item__content {
+    width: 150px;
+  }
+  // 标题的盒子
+   .el-form--inline .el-form-item.title-item .el-form-item__content{
+    width: 398px;
+  }
+  .top-card .el-card__body {
+    padding-left: 11px;
+  }
+  .el-form-item__label {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+}
 </style>
