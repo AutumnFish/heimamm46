008553ad4afbf8566493d9a87d9663b839eed6cc
autumnFish <517729329@qq.com>
2020-02-20 11:47:02 +0800

    题库模块 - 顶部布局1

--- src/views/index/question/components/questionAdd.vue
+++ src/views/index/question/components/questionAdd.vue
@@ -1,14 +1,18 @@
 <template>
   <el-dialog class="question-add" fullscreen title="新增题库" :visible.sync="dialogFormVisible">
     <el-form :model="form">
-      <el-form-item label="活动名称" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+      <el-form-item label="学科" :label-width="formLabelWidth">
+        <subjectSel v-model="form.subject" />
       </el-form-item>
-      <el-form-item label="活动区域" :label-width="formLabelWidth">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+      <el-form-item label="阶段" :label-width="formLabelWidth">
+        <el-select v-model="form.step" placeholder="请选择阶段">
+          <el-option label="初级" :value="1"></el-option>
+          <el-option label="中级" :value="2"></el-option>
+          <el-option label="高级" :value="3"></el-option>
         </el-select>
+      </el-form-item>
+      <el-form-item label="企业" :label-width="formLabelWidth">
+        <enterpriseSel v-model="form.enterprise" />
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
@@ -19,23 +23,41 @@
 </template>
 
 <script>
+// 导入组件
+import enterpriseSel from './enterpriseSel.vue';
+import subjectSel from './subjectSel.vue';
 export default {
+  // 注册组件
+  components: {
+    enterpriseSel,
+    subjectSel
+  },
   data() {
     return {
       dialogFormVisible: false,
       // 表单的数据
-      form: {},
-      formLabelWidth:"120px"
+      form: {
+        // 阶段
+        setp: '',
+        // 学科和企业
+        subject: '',
+        enterprise: ''
+      },
+      formLabelWidth: '120px'
     };
   }
 };
 </script>
 
 <style lang="less">
-.question-add{
-    .el-form{
-        width: 835px;
-        margin: 0 auto; 
-    }
+.question-add {
+  .el-form {
+    width: 835px;
+    margin: 0 auto;
+  }
+  // 选择框宽度
+  .el-select {
+    width: 364px;
+  }
 }
 </style>
