2fd928a1d875175dfb8324c42acf1c4794e7d6c9
autumnFish <517729329@qq.com>
2020-02-21 16:51:41 +0800

    统计模块 - 底部布局

--- src/views/index/chart/chart.vue
+++ src/views/index/chart/chart.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class='chart-container'>
+  <div class="chart-container">
     <el-card class="top-card">
       <ul>
         <li>
-          <span class="circle primary">{{topData.increment_users }}</span>
+          <span class="circle primary">{{ topData.increment_users }}</span>
           <span>今日新增用户</span>
         </li>
         <li>
-          <span class="circle primary">{{topData.total_users}}</span>
+          <span class="circle primary">{{ topData.total_users }}</span>
           <span>总用户量</span>
         </li>
         <li>
-          <span class="circle warning">{{topData.increment_questions }}</span>
+          <span class="circle warning">{{ topData.increment_questions }}</span>
           <span>新增试题</span>
         </li>
         <li>
@@ -19,82 +19,140 @@
           <span>总试题量</span>
         </li>
         <li>
-          <span class="circle success">{{topData.total_done_questions}}</span>
+          <span class="circle success">{{ topData.total_done_questions }}</span>
           <span>总刷题量</span>
         </li>
         <li>
-          <span class="circle success">{{topData.personal_questions }}</span>
+          <span class="circle success">{{ topData.personal_questions }}</span>
           <span>人均刷题量</span>
         </li>
-        
       </ul>
+    </el-card>
+    <el-card class="bottom-card">
+      <div ref="chart" class="chart-box"></div>
     </el-card>
   </div>
 </template>
 
 <script>
 // 导入接口
-import {titleData} from '@/api/data.js'
+import { titleData } from '@/api/data.js';
+// 导入 echarts
+import echarts from 'echarts';
 export default {
-    name:"chart",
-    data(){
-      return {
-        topData:{}
-      }
-    },
-    // 获取数据
-    created() {
-      titleData().then(res=>{
-        // window.console.log(res)
-        this.topData = res.data;
-      })
-    },
-}
+  name: 'chart',
+  data() {
+    return {
+      topData: {}
+    };
+  },
+  // 获取数据
+  created() {
+    titleData().then(res => {
+      // window.console.log(res)
+      this.topData = res.data;
+    });
+  },
+  // echarts的渲染依赖于 dom加载
+  mounted() {
+    const myChart = echarts.init(this.$refs.chart);
+    const option = {
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: {c} ({d}%)'
+      },
+      legend: {
+        orient: 'vertical',
+        right: 10,
+        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+      },
+      color: ['#429dfe', '#f86137', '#aa314d'],
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            normal: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              show: true,
+              textStyle: {
+                fontSize: '30',
+                fontWeight: 'bold'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          data: [
+            { value: 335, name: '直接访问' },
+            { value: 310, name: '邮件营销' },
+            { value: 234, name: '联盟广告' }
+          ]
+        }
+      ]
+    };
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+  }
+};
 </script>
 
 <style lang="less">
-  .chart-container{
-    .top-card{
-      height: 150px;
-      margin-bottom: 12px;
-      ul{
-        display: flex;
-        align-items: center;
-        padding: 0;
-        margin: 0;
-        justify-content: space-around;
-        list-style: none;
-        li{
-          span{
-            display: block;
-            color:#737373;
-            text-align: center;
-            font-size:16px;
+.chart-container {
+  .top-card {
+    height: 150px;
+    margin-bottom: 12px;
+    ul {
+      display: flex;
+      align-items: center;
+      padding: 0;
+      margin: 0;
+      justify-content: space-around;
+      list-style: none;
+      li {
+        span {
+          display: block;
+          color: #737373;
+          text-align: center;
+          font-size: 16px;
+        }
+        .circle {
+          width: 99px;
+          height: 99px;
+          border: 2px solid orange;
+          border-radius: 50%;
+          font-size: 35px;
+          text-align: center;
+          line-height: 99px;
+          &.primary {
+            color: #409efe;
+            border-color: #409efe;
           }
-          .circle{
-            width: 99px;
-            height: 99px;
-            border:2px solid orange;
-            border-radius: 50%;
-            font-size:35px;
-            text-align: center;
-            line-height: 99px;
-            &.primary{
-              color:#409efe;
-              border-color: #409efe;
-            }
-            &.warning{
-              color:#f86137;
-              border-color:#f86137;
-            }
-            &.success{
-              color:#70c58b;
-              border-color:#70c58b;
-            }
+          &.warning {
+            color: #f86137;
+            border-color: #f86137;
           }
-        
+          &.success {
+            color: #70c58b;
+            border-color: #70c58b;
+          }
         }
       }
     }
   }
+  .bottom-card {
+    height: 571px;
+  }
+  .chart-box{
+    height: 571px;
+  }
+}
 </style>
