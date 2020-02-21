13c544c40ca4bfff209773e913a0addf7acff86f
autumnFish <517729329@qq.com>
2020-02-21 11:36:11 +0800

    题库模块 - 视频上传功能整合

--- src/views/index/question/components/questionAdd.vue
+++ src/views/index/question/components/questionAdd.vue
@@ -106,7 +106,23 @@
           placeholder=""
         ></el-input>
       </el-form-item>
-
+      <!-- 分割线 -->
+      <el-divider></el-divider>
+      <el-form-item label="解析视频" :label-width="formLabelWidth">
+        <el-upload
+          class="avatar-uploader"
+          :action="uploadURL"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <el-button size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">
+            只能上传视频文件，且不超过2mb
+          </div>
+        </el-upload>
+        <video class="video" :src="videoURL" controls></video>
+      </el-form-item>
       <!-- 分割线 -->
       <el-divider></el-divider>
       <!-- 富文本编辑器1 标题 -->
@@ -182,13 +198,38 @@ export default {
             text: '炸酱面',
             image: 'upload/20191129/4067f19ab53a5e8388ad3459e23110f0.jpeg'
           }
-        ]
+        ],
+        // 解析视频
+        video: ''
       },
-
-      formLabelWidth: '120px'
+      formLabelWidth: '120px',
+      // 本地的视频预览地址
+      videoURL: '',
+      // 上传地址
+      uploadURL: process.env.VUE_APP_URL + '/question/upload'
     };
   },
-  methods: {}
+  methods: {
+    handleAvatarSuccess(res, file) {
+      // 本地的视频预览地址
+      this.videoURL = URL.createObjectURL(file.raw);
+      // window.console.log(res);
+      // 保存到提交的数据中
+      this.form.video = res.data.url;
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'video/mp4';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('上传只能是视频格式！');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传视频大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
+    }
+  }
 };
 </script>
 
@@ -205,6 +246,11 @@ export default {
   // 级联选择器的宽度
   .el-cascader {
     width: 364px;
+  }
+  // 预览视频的样式
+  .video{
+    width: 360px;
+    height: 240px;
   }
 }
 </style>
